eda3efbf2d6a815aa463d46a1625ce8e82e3fe17
Januar Maksum <januar@alodokter.com>
2022-08-02 09:20:24 +0700

    init project

---
+++ asset/js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 40,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    grabCursor: true,
+});
+
+function hoverCardHandler(e) {
+    console.log(e)
+}
+
+function hoverOutCardHandler(e) {
+    console.log(e)
+}
